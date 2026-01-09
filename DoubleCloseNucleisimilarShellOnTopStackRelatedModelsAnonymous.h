cc4915ddbebb86b7e9603c97b068bd29032d7a8b
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2026-01-09 08:51:01 +0100

    Update DoubleCloseNucleisimilarShellOnTopStackRelatedModelsAnonymous.h

--- DoubleCloseNucleisimilarShellOnTopStackRelatedModelsAnonymous.h
+++ DoubleCloseNucleisimilarShellOnTopStackRelatedModelsAnonymous.h
@@ -1,1 +1,2 @@
-
+//we do not need to shutdown models used in a stack here. But, for similarities preseerve, we d better use half edged stacks which is strictly not related to
+//chess engine decision but is related to a futur behaviour of some count of models that cannot be clamped as datas on a stack. Then, abstract, datas are independants (another field of science).
