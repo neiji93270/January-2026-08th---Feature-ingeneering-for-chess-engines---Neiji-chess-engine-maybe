c945ddb7c41127f4d099f87f4ce0d74cca0b0aec
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2026-01-09 07:04:30 +0100

    Create Atomic.h

---
+++ Atomic.h
@@ -0,0 +1,10 @@
+//naming classes with jammin behaviour
+//will let us dropping tables in a cipher or multiple layers of ciphers.
+///we can prevent this two step behaviour with a single behaviour 
+//here, the gozl is again to track datas but similarly
+//we dont need to create get set methods.
+//internally, plus, for this file, we want to be able to decide with atomic
+//behaviour of patterns we want. this is an ai behaviour but
+//with several latents variables. so, subtresholds are needed to create get znd set methods.
+///then, we want a success method elsewhere in other clzsses
+//which would let algorithms with orbital and sampling methods.
