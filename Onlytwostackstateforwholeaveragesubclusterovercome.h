7839b243209cc372631168506ea972ac1aabbf85
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2026-01-09 10:10:32 +0100

    Create Onlytwostackstateforwholeaveragesubclusterovercome.h

---
+++ Onlytwostackstateforwholeaveragesubclusterovercome.h
@@ -0,0 +1,8 @@
+//there is nonreal ways to overcome clusters algebras.
+//so here we prefer to get and set states on stack that are higher rated
+//than a common stack associated with fifo 
+//so the tree rely on sevveral clusters  then the simpliest 
+//and although complex algebras ronise here are clifford algebra and not
+//lie algebra. clifford algebras would be transformd to stack tracing elements
+//by an heuristic. this heuristic id an atomic heuristic and complex enough
+//to set datas on lower stack traces. mathematically said, we must use non real numbers generators.
